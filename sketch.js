9e3bda7f032cbbcf9e8853efa5062b5d581656bc
mattholamieux <50525591+mattholamieux@users.noreply.github.com>
2024-10-30 12:12:46 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,13 +11,13 @@ function draw() {
     textSize(40);
     text(currentScene.text, width / 2, 100, 600);
     textSize(30);
-    text(currentScene.choiceA, (width / 2) - 200, 600, 300);
-    text(currentScene.choiceB, (width / 2) + 200, 600, 300);
+    text(currentScene.choiceA, (width / 2) - 200, 500, 300);
+    text(currentScene.choiceB, (width / 2) + 200, 500, 300);
     textSize(20);
     if (currentScene == start) {
         text("press any key", width / 2, 300, 300);
     } else if (currentScene == walk || currentScene == openWindow || currentScene == stove || currentScene == run) {
-        text("the end", width / 2, 600, 300);
+        text("the end", width / 2, 500, 300);
     } else {
         text("use number keys to choose your next move", width / 2, 800, 600);
     }
